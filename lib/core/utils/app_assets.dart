b69afe05eed95f476dfaec508ad369d157589bc8
Fatima Klay <fatimaklay2002@gmail.com>
2026-05-30 18:51:29 +0300

    app colors

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesForgetPassword
+  /// assets/images/forget_password.png
+  static const String assetsImagesForgetPassword = "assets/images/forget_password.png";
+
+  /// Assets for assetsImagesOnboarding1
+  /// assets/images/onboarding1.png
+  static const String assetsImagesOnboarding1 = "assets/images/onboarding1.png";
+
+  /// Assets for assetsImagesOnboarding2
+  /// assets/images/onboarding2.png
+  static const String assetsImagesOnboarding2 = "assets/images/onboarding2.png";
+
+  /// Assets for assetsImagesOnboarging3
+  /// assets/images/onboarging3.png
+  static const String assetsImagesOnboarging3 = "assets/images/onboarging3.png";
+
+  /// Assets for assetsImagesSucccesOrder
+  /// assets/images/Succces_order.png
+  static const String assetsImagesSucccesOrder = "assets/images/Succces_order.png";
+
+  /// Assets for assetsImagesVector
+  /// assets/images/Vector.svg
+  static const String assetsImagesVector = "assets/images/Vector.svg";
+}
+
